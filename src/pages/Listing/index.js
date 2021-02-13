fbc9503801260953796cd35787d10f360f6920b8
Chiamaka Ikeanyi <chiamaka.ikeanyi@gmail.com>
2021-02-13 19:14:56 +0100

    Remove unused import

--- src/pages/Listing/index.js
+++ src/pages/Listing/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Search from '../../components/Search';
 import DefaultLayout from '../../components/DefaultLayout';
 import styles from './listing.module.scss';
